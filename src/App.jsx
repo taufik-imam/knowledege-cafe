0577643d39419faeb4c0c0e939fab1be91a79bbe
taufik-imam <taufikimamhridoy@gmail.com>
2023-09-12 17:47:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs'
+
+function App() {
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Blogs></Blogs>
+      
+      
+    </>
+  )
+}
+
+export default App
